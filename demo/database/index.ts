a756bb9d2294411d490d0182a69040172425d641
PPz <ppzreboot@qq.com>
2024-12-24 23:58:27 +0800

    docs: readme and demo

--- demo/database/index.ts
+++ demo/database/index.ts
@@ -1,6 +1,8 @@
 export
 async function init_db() {
+  console.log('initializing database connection')
   await new Promise(res =>
     setTimeout(res, 1000)
   )
+  console.log('database is ok')
 }
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Boor
+
+## Run Demo
+``` bash
+git clone git@github.com:ppzreboot/boor.git
+cd boor
+deno task demo
+```
